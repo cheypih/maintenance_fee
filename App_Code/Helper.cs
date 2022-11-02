00a60ee2612f33aab3556c46f2574e836ee71a61
gnewnhoj <117284341+gnewnhoj@users.noreply.github.com>
2022-11-02 21:11:12 +0800

    Add files via upload

---
+++ App_Code/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+using System.Data;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+
+
+
+namespace Maintenance_Fee
+{
+    public class Helper
+    {
+        public static string GetCon()
+        {
+            return ConfigurationManager.ConnectionStrings["MyCon"].ConnectionString;
+        }
+        public static string CreateSHAHash(string Phrase)
+        {
+            SHA512Managed HashTool = new SHA512Managed();
+            Byte[] PhraseAsByte = System.Text.Encoding.UTF8.GetBytes(string.Concat(Phrase));
+            Byte[] EncryptedBytes = HashTool.ComputeHash(PhraseAsByte);
+            HashTool.Clear();
+            return Convert.ToBase64String(EncryptedBytes);
+        }
+    }
+}
